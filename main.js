04b40dc37935dc582bc2d6ebc7f503d7e9d8f928
Jibyjojan <jibyjojan@yahoo.com>
2019-03-31 10:17:31 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -23,9 +23,8 @@
 //    If the user clicks "Add New" and the input is empty, 
 //    the new item's title needs to default to Title {number} (ex: Title 6 or Title 7)
 //    depending on the number of items already on the page
-
 $(document).ready(function(){
-
+var itemno=4;
 $("#title1").click(function(){
    	 	$("#description1").toggle();
   });
@@ -41,7 +40,6 @@ $("#title1").click(function(){
  $("#title4").click(function(){
    	 	$("#description4").toggle();
   });
-
 $("#close1").click(function(){
   $("#item1").remove();
  });
@@ -58,5 +56,11 @@ $("#close4").click(function(){
     $("#item4").remove();
   });
 
+$(".add").click(function(){
+var newt = $('#newtitle').val();
+var html='';
+html='<div class="item"id="item5"><h1 class="title"id="title5">'+newt+'</h1><span class="close" id="close5">x</span><p class="description"id="description5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id scelerisque lacus, rutrum lobortis nisl. Ut dapibus, quam in auctor pharetra, nibh erat pharetra velit, vitae tristique mauris dui quis.</p></div>';
 
+    $(".item-list").append(html);
+});
 });
